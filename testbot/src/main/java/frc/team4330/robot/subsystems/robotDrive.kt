fd796aa8a46ce3fcc867b947cbd67c823775c20e
Sithija Gunasinghe <sparks10.sg@gmail.com>
2018-01-06 08:34:04 -0600

    fixed talon stuff for the v5 library

--- testbot/src/main/java/frc/team4330/robot/subsystems/robotDrive.kt
+++ testbot/src/main/java/frc/team4330/robot/subsystems/robotDrive.kt
@@ -2,23 +2,24 @@ package frc.team4330.robot.subsystems
 
 import frc.team4330.robot.IO.Input
 import frc.team4330.robot.IO.RobotMap
+import frc.team4330.robot.drivebase.Talon
 import frc.team4330.robot.drivebase.Talons
 import frc.team4330.robot.drivebase.Victor
 
 class robotDrive : SubsystemBase() {
 
-    private val rightTal: Talons
-    private val leftTal: Talons
+    private val rightTal: Talon
+    private val leftTal: Talon
     private val rightVic: Victor
     private val leftVic: Victor
     private var reverse: Boolean = false
 
     init {
         rightVic = Victor(RobotMap.RIGHT_VICTOR_PWM)
-        rightTal = Talons(RobotMap.RIGHT_TALON)
+        rightTal = Talon(RobotMap.RIGHT_TALON)
 
         leftVic = Victor(RobotMap.LEFT_VICTOR_PWM)
-        leftTal = Talons(RobotMap.LEFT_TALON)
+        leftTal = Talon(RobotMap.LEFT_TALON)
     }
 
     fun autoDrive(left: Double, right: Double) {
