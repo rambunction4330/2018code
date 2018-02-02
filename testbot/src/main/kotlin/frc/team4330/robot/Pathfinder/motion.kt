336e41ce1923d0b1d42df6062fa7cccdece83468
Sithija Gunasinghe <sparks10.sg@gmail.com>
2018-02-02 15:52:28 -0600

    Added more motion profiling stuff

--- testbot/src/main/kotlin/frc/team4330/robot/Pathfinder/motion.kt
+++ testbot/src/main/kotlin/frc/team4330/robot/Pathfinder/motion.kt
@@ -1,6 +1,7 @@
 package frc.team4330.robot.Pathfinder
 
 import frc.team4330.robot.IO.RobotMap
+import frc.team4330.robot.subsystems.NavX
 import jaci.pathfinder.Pathfinder
 import jaci.pathfinder.Trajectory
 import jaci.pathfinder.followers.EncoderFollower
@@ -8,15 +9,24 @@ import java.io.File
 
 class motion {
 
-
+    private val gyro: NavX = NavX()
     val fileleft: File = File("test2_left.csv")
     val fileright: File = File("test2_right.csv")
 
     val rightTraj: Trajectory = Pathfinder.readFromCSV(fileright)
     val leftTraj: Trajectory = Pathfinder.readFromCSV(fileleft)
 
-    val leftFollow: EncoderFollower = EncoderFollower(leftTraj)
-    val rightFollow: EncoderFollower = EncoderFollower(rightTraj)
+    var leftFollow: EncoderFollower = EncoderFollower(leftTraj)
+    var rightFollow: EncoderFollower = EncoderFollower(rightTraj)
+
+    val l = leftFollow.calculate(RobotMap.leftEncPos)
+    val r = rightFollow.calculate(RobotMap.rightEncPos)
+
+    var heading = gyro.angle()
+    var desired_headingL = Pathfinder.r2d(leftFollow.heading)
+
+    var angleDifference = Pathfinder.boundHalfDegrees(desired_headingL - heading)
+    var turn = .8 * (-1.0 / 80) * angleDifference
 
     init {
         leftFollow.configureEncoder(RobotMap.leftEncPos, 1024, 0.1016)
