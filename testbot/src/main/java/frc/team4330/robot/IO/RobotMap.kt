0d5f588c32ef08930e16c14992df3eb6d0db13f1
Sithija Gunasinghe <sparks10.sg@gmail.com>
2018-02-08 17:07:54 -0600

    Changed code for testbot

--- testbot/src/main/java/frc/team4330/robot/IO/RobotMap.kt
+++ testbot/src/main/java/frc/team4330/robot/IO/RobotMap.kt
@@ -1,19 +1,16 @@
 package frc.team4330.robot.IO
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX
 
 
 object RobotMap {
-    val RIGHT_VICTOR_1: WPI_VictorSPX = WPI_VictorSPX(1)
-    val RIGHT_VICTOR_2: WPI_VictorSPX = WPI_VictorSPX(4)
-    val LEFT_VICTOR_1: WPI_VictorSPX = WPI_VictorSPX(2)
-    val LEFT_VICTOR_2: WPI_VictorSPX = WPI_VictorSPX(3)
-    val LEFT_TALON = 2
-    val RIGHT_TALON = 3
+    val RIGHT_VICTOR_1: WPI_TalonSRX = WPI_TalonSRX(1)
+    val RIGHT_VICTOR_2: WPI_VictorSPX = WPI_VictorSPX(2)
+    val LEFT_VICTOR_1: WPI_TalonSRX = WPI_TalonSRX(3)
+    val LEFT_VICTOR_2: WPI_VictorSPX = WPI_VictorSPX(4)
 
     val DRIVE_JOYSTICK = 0
-
-    val PCM_CAN = 0
 
     val PISTON_ONE_0 = 0
     val PISTON_ONE_1 = 1
--- testbot/src/main/java/frc/team4330/robot/Robot.kt
+++ testbot/src/main/java/frc/team4330/robot/Robot.kt
@@ -2,7 +2,6 @@ package frc.team4330.robot
 
 import edu.wpi.first.wpilibj.CameraServer
 import edu.wpi.first.wpilibj.IterativeRobot
-import edu.wpi.first.wpilibj.command.Scheduler
 import frc.team4330.robot.IO.Input
 import frc.team4330.robot.IO.RobotMap
 import frc.team4330.robot.subsystems.robotDrive
@@ -12,30 +11,21 @@ class Robot : IterativeRobot() {
     companion object {
         val xbox: Input = Input(RobotMap.DRIVE_JOYSTICK)
 
-
-//        val comp: Compressor = Compressor(0)
-//        val pist1: SingleSolenoid = SingleSolenoid(0)
-//        val pist2: SingleSolenoid = SingleSolenoid(1)
-
         val mDrive: robotDrive = robotDrive()
     }
 
-    private lateinit var scheduler: Scheduler
 
     override fun robotInit() {
         CameraServer.getInstance().startAutomaticCapture()
     }
 
     override fun disabledInit() {
-//        comp.stop()
     }
 
     override fun autonomousInit() {
-//        comp.start()
     }
 
     override fun teleopInit() {
-//        comp.start()
     }
 
     override fun testInit() {
@@ -48,7 +38,6 @@ class Robot : IterativeRobot() {
 
     override fun teleopPeriodic() {
         mDrive.tankDrive(xbox)
-
     }
 
     override fun testPeriodic() {}
